1a247be948b0e92520e0f7da89e838b7082f07c1
Jiwoo <2023052@bssm.hs.kr>
2023-03-28 20:33:45 +0900

    bananamilk

--- strawberrymilk.c
+++ strawberrymilk.c
@@ -2,8 +2,10 @@
 
 int main(){
 	printf("I love bananamilk!!!!!!");
-	printf("But..I can't drink bananamilk now..")
+	printf("But..I can't drink bananamilk now..");
 	printf("So I am sad");
+	printf("But yehen give chocolatemilk to me ");
+	printf("So I am very happy!!");
 
 	return 0;
 }	
